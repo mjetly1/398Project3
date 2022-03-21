35a0e3a2b0c848d2d4ee5ea6956433bb6f6119d7
mjetly1 <98354078+mjetly1@users.noreply.github.com>
2022-03-21 16:06:24 -0400

    updated Flip code

--- flipBit.py
+++ flipBit.py
@@ -1,10 +1,23 @@
 import mraa
 
-gpio23 = mraa.Gpio(23)
-gpio23.dir(mraa.DIR_IN)
+def main():
+    
+    gpio23 = mraa.Gpio(23)
+    gpio23.dir(mraa.DIR_OUT)
+    gpio23.write(1)
 
-while(1):
-    print(gpio23.read())
+    while(1):
+        value = gpio23.read()
+        if value == 1:
+            print("1")
+            gpio23.write(0)
+
+        else:
+            print("0")
+            gpio23.write(1)
+
+if __name__ == '__main__' :
+    main()
 
 
     
